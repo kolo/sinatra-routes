f0c5ee88f7678fcf4028c7eab7db811bbb9f743c
Dmitry Maksimov <dmtmax@gmail.com>
2010-08-18 12:04:40 +0400

    Changed rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -8,6 +8,8 @@ begin
     gemspec.email = "dmtmax@gmail.com"
     gemspec.homepage = "http://github.com/kolo/sinatra-routes"
     gemspec.authors = ["Dmitry Maksimov"]
+    gemspec.files = FileList["README.md", "rakefile.rb", "example/**/*", "lib/**/*"]
+    gemspec.add_dependency "sinatra", ">= 1.0"
   end
 rescue LoadError
   puts "Jeweler is not available. Install it with: gem install jeweler"
